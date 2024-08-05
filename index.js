f7be8af76b76ce34526df43a52ff22eead2c73c6
Rishi Sharma <rishisharma222002@gmail.com>
2024-08-05 11:29:53 +0530

    make a path to check the server status.

--- index.js
+++ index.js
@@ -9,4 +9,15 @@ app.listen(port,()=>{
 
 app.use(express.urlencoded({extended:true}));               // use to decript the input or 
 
-app.set("view engine","ejs");
+app.set("view engine","ejs");                               // set the view engine.
+
+// the main file are at views folder.
+
+app.set("views",path.join(__dirname,"views"));                  // set the views folder.
+app.use(express.static(path.join(__dirname,"public")));         // set the local files. 
+
+// Check server
+
+app.get("/",(req,res)=>{
+    res.send("Server working well !");
+});
